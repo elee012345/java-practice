d05008938fef876b13e4aa674b19ae0f3318b5ac
Ethan Lee <ethanlee012345@gmail.com>
2022-05-24 12:15:13 -0500

    threads are working

--- practice 2/ThreadPractice.java
+++ practice 2/ThreadPractice.java
@@ -4,13 +4,15 @@ import java.io.*;
 public class ThreadPractice implements Runnable {
 
     public static void main( String args[] ) {
-        Runnable RunnableInterface = new ThreadPractice();
-        Thread AThread = new Thread(RunnableInterface, "Thread 1");
-        AThread.start();
+        ThreadPractice runnable = new ThreadPractice();
+        Thread thread1 = new Thread(runnable, "Thread 1");
+        thread1.start();
     }
 
     public void run() {
-        System.out.println("Hi");
+        for ( int i = 0; i < 10; i++ ) {
+            System.out.println(Thread.currentThread().getName() + " is on its " + i + " iteration.");
+        }
     }
 
 }   
