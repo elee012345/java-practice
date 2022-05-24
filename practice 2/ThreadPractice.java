40e14a3b70d3a76a7cc1ff74542015be9fc837a3
Ethan Lee <ethanlee012345@gmail.com>
2022-05-24 12:18:02 -0500

    running at the same time!

--- practice 2/ThreadPractice.java
+++ practice 2/ThreadPractice.java
@@ -6,12 +6,19 @@ public class ThreadPractice implements Runnable {
     public static void main( String args[] ) {
         ThreadPractice runnable = new ThreadPractice();
         Thread thread1 = new Thread(runnable, "Thread 1");
+        Thread thread2 = new Thread(runnable, "Thread 2");
         thread1.start();
+        thread2.start();
     }
 
     public void run() {
         for ( int i = 0; i < 10; i++ ) {
             System.out.println(Thread.currentThread().getName() + " is on its " + i + " iteration.");
+            try {
+                Thread.currentThread().sleep(1000);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
         }
     }
 
