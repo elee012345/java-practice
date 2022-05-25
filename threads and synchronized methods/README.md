a4b09e870480e72b91cd4d2e0430111ccfaff11a
Ethan Lee <ethanlee012345@gmail.com>
2022-05-25 17:55:01 -0500

    added readme

---
+++ threads and synchronized methods/README.md
@@ -0,0 +1,2 @@
+this man is amazing at explaining threads in detail
+https://www.youtube.com/watch?v=mTGdtC9f4EU&list=PLL8woMHwr36EDxjUoCzboZjedsnhLP1j4&index=1
---
+++ threads and synchronized methods/synchronized methods/synchronized.java
@@ -0,0 +1,6 @@
+// i like this vid
+// https://www.youtube.com/watch?v=71dgtPrbToE
+// also more epic vid: https://www.youtube.com/watch?v=eKWjfZ-TUdo&list=PLL8woMHwr36EDxjUoCzboZjedsnhLP1j4&index=5
+
+// am kind of bored so i might just do this later
+// this is kind of a bigger project
